e7a394c012e2ce27a21b04d4a2759b4921b09299
Bruno Guardiani <brunodipaolo13@gmail.com>
2020-11-11 12:38:37 -0300

    findById Impementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,10 +9,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj = new Department (1, "Books");
+		/*Department obj = new Department (1, "Books");
 		System.out.println(obj);
-		Seller seller = new Seller (21, "Pierre", "pierre@gmail.com", new Date(), 3000.0, obj);
+		Seller seller = new Seller (21, "Pierre", "pierre@gmail.com", new Date(), 3000.0, obj); TESTANDO AS CLASSES*/
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
