d10277bbc9f00231648226a38aaf22332496620c
Bruno Guardiani <brunodipaolo13@gmail.com>
2020-11-11 23:35:10 -0300

    insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,6 +28,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller Insert ===");
+		Seller newSeller = new Seller(null, "Yuri", "yuri.mello@ufba.br", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
